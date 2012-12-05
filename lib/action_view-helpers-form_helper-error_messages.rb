83a9fae5d4e6cbf12897a5af4a1cd62eafa90f75
yalab <rudeboyjet@gmail.com>
2012-12-05 21:18:42 +0900

    Rename files.

--- lib/action_view-helpers-form_helper-error_messages.rb
+++ lib/action_view-helpers-form_helper-error_messages.rb
@@ -1,11 +1,2 @@
-require "action_view-helpers-form_helper-error_messages/version"
-
-module ActionView
-  module Helpers
-    module FormHelper
-      module ErrorMessages
-        # Your code goes here...
-      end
-    end
-  end
-end
+require "action_view/helpers/form_helper/error_messages/version"
+require "action_view/helpers/form_helper/error_messages"
---
+++ lib/action_view/helpers/form_helper/error_messages.rb
@@ -0,0 +1,9 @@
+module ActionView
+  module Helpers
+    module FormHelper
+      module ErrorMessages
+        # Your code goes here...
+      end
+    end
+  end
+end
